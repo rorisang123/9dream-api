9289097a2d6c5c0d63749a3bd4bf20b0b644c632
Rorisang <roriiisang@gmail.com>
2024-02-29 19:38:02 +0200

    Endpoints skeleton

---
+++ index.js
@@ -0,0 +1,54 @@
+/* Entities
+const UserModel = {};
+const organisationUserModel = {}
+const campaignerUserModel = {}
+const voterUserModel = {}
+const expertUserModel = {}
+const voteModel = {}
+const transactionModel = {}
+const promiseModel = {}
+const reviewModel = {}
+const campaignModel = {}
+*/
+
+const express = require('express')
+const bodyParser = require('body-parser')
+var cors = require('cors')
+const app = express()
+const db = require('./queries')
+const port = 3000
+
+app.use(cors())
+
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+
+app.get('/', (request, response) => {
+  response.json({ info: 'Node.js, Express, and Postgres API' })
+})
+
+app.get('/users', db.getUsers)
+app.get('/users/:id', db.getUserById)
+app.post('/users', db.createUser)
+app.put('/users/:id', db.updateUser)
+app.delete('/users/:id', db.deleteUser)
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`)
+})
+
+
+// Error-handling Middleware
+app.use((req, res, next) => {
+  const error = new Error('Something went wrong');
+  next(error);
+});
+
+app.use((err, req, res, next) => {
+  console.error('Error:', err.message);
+  res.status(500).send('Internal Server Error');
+});
